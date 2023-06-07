203d729c8cd9d74f00f8eb1e4b2a5cf86ee58631
Ivan Bueno <ivan.bueno@jud.ca.gov>
2023-06-06 17:53:13 -0700

    Search for filesystem path notation in the body and replace with relative urls

--- datacrawler/spiders/datacrawler.py
+++ datacrawler/spiders/datacrawler.py
@@ -3,6 +3,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ..items import DatacrawlerItem
 from configparser import ConfigParser  # ver. < 3.0
+from urllib.parse import urlparse
 
 class DatacrawlerSpider(CrawlSpider):
     name = 'datacrawler'
@@ -37,6 +38,45 @@ class DatacrawlerSpider(CrawlSpider):
         item = DatacrawlerItem()
         item['url'] = response.url
 
+        # Replace filesystem path notations with relative url paths.
+        url = urlparse(response.url)
+        directories = list(filter(None, url.path.split('/')))
+        del directories[-1]
+        ancestorCount = len(directories)
+
+        # Generate a list of search/replace tuples.
+        #
+        # Example for a/b/c:
+        #
+        # [
+        #     ('../../../', '/')
+        #     ('../../', 'a/')
+        #     ('../', 'a/b/')
+        # ]
+        REPLACEMENTS = []
+
+        # Generate Search and Replace tuple for the max number of ancestors
+        # for this page, ie. tuple('../../../', '/') for /a/b/c
+        search_string = ''
+        for i in range(ancestorCount):
+            search_string += '../'
+        REPLACEMENTS.append((search_string, '/'))
+
+        # Generate tuple for lower level parents
+        replace_string = ''
+        dir_count = 1
+        for i in directories:
+            search_string = ''
+            for k in range(ancestorCount - dir_count):
+                search_string += '../'
+
+            replace_string += '/' + i
+
+            REPLACEMENTS.append((search_string, replace_string + '/'))
+            dir_count += 1
+
+        del REPLACEMENTS[-1]
+
         try:
             item['title'] = response.css(self.title_v).get().strip().title()
         except AttributeError:
@@ -44,6 +84,8 @@ class DatacrawlerSpider(CrawlSpider):
 
         try:
             item['body'] = response.css(self.body_v).get().strip()
+            for search, replace in REPLACEMENTS:
+                item['body'] = item['body'].replace(search, replace)
         except AttributeError:
             item['body'] = None
 
