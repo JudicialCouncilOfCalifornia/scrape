3c27d930f5347fcf6b5fe0cbbba1903a3265bcd2
Ivan Bueno <ivan.bueno@jud.ca.gov>
2023-08-30 16:59:32 -0700

    update dca page json

--- datacrawler/spiders/dca-page-cleanup.py
+++ datacrawler/spiders/dca-page-cleanup.py
@@ -9,7 +9,7 @@ def parseJson(filename):
         data = json.load(jsonFile)
 
     for i in range(len(data)):
-        html = BeautifulSoup(str(data[i]['body']), 'lxml')
+        html = BeautifulSoup(str(data[i]['body']).encode(), 'lxml')
         html.find('div', id='leftNavDropdown').decompose()
         if html.find('div', id='printWrap'):
             html.find('div', id='printWrap').decompose()
@@ -17,7 +17,7 @@ def parseJson(filename):
             html.find('div', id='mobileFloatRight').decompose()
         html.find('div', id='leftPanel').decompose()
 
-        data[i]['body'] = str(html.find('div', id='mainContent'))
+        data[i]['body'] = str(html.find('div', id='mainContent')).replace('\\n', ' ').replace('\\r', '').replace('\\t', '')
 
     with open("../../results/dca-page-clean.json", "w") as jsonFile:
         json.dump(data, jsonFile)
